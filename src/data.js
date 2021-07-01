b5bc5c9ba3d4a654ff4831ea52725cbb580d1e22
vasam <vasanthmareboina98@gmail.com>
2021-07-01 15:09:45 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+
+
+const subscribedBooks = [
+    {
+        "name" : 'JAVA',
+        "author" : 'Bala',
+        "read" : false
+    },
+    {
+        "name" : 'C',
+        "author" : 'Bala',
+        "read" : false
+    },
+    {
+        "name" : 'C++',
+        "author" : 'Bala',
+        "read" : false
+    },
+
+  ];
+
+
+const books = [
+    {
+        "name" : 'Python',
+        "author" : 'Bala',
+        "read" : false
+    },
+    {
+        "name" : 'React',
+        "author" : 'Bala',
+        "read" : false
+    },
+    {
+        "name" : 'JS',
+        "author" : 'Bala',
+        "read" : false
+    },
+  ]
